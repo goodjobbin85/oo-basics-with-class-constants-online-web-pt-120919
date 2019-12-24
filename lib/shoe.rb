6d23627b62d51b5e0107c2f9193cd2a4536a731e
thomas dubiel <td1729@hotmail.com>
2019-12-24 17:43:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,10 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand 
-    BRANDS << brand
+    
+    if !BRANDS.include?(brand)
+      BRANDS << brand 
+    end
   end
 
   def cobble
